ffa9c5987181d5b1890143383668ec7c06fb2b76
SadiaUrooj <sadiaurooj258@gmail.com>
2025-08-22 17:57:58 +0500

    Project files added

---
+++ NZWalks/NZWalks.API/Models/DTO/AddWalkRequestDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWalks.API.Models.DTO
+{
+    public class AddWalkRequestDto
+    {
+        [Required]
+        [MaxLength(100, ErrorMessage = "Name should be of maximum 100 characters")]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(1000)]
+        public string Description { get; set; }
+        [Required]
+        [Range(0,50)]
+        public double lengthInKm { get; set; }
+
+        public string? WalkImageUrl { get; set; }
+
+        //Relationship b/w the walk and region
+        [Required]
+        public Guid DifficultyId { get; set; }
+        [Required]
+        public Guid RegionId { get; set; }
+    }
+}
